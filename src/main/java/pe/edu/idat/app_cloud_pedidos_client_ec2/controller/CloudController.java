df60babb4ca4c64edcf636133c59cde897630c04
JoseRaul14 <norona.gomez.jos@gmail.com>
2025-02-23 16:13:24 -0500

    este commit pertenece a jose noroña

--- src/main/java/pe/edu/idat/app_cloud_pedidos_client_ec2/controller/CloudController.java
+++ src/main/java/pe/edu/idat/app_cloud_pedidos_client_ec2/controller/CloudController.java
@@ -1,8 +1,7 @@
 package pe.edu.idat.app_cloud_pedidos_client_ec2.controller;
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.idat.app_cloud_pedidos_client_ec2.service.ServicePagosCliente;
 
@@ -27,5 +26,10 @@ public class CloudController {
     @GetMapping("/pago-pedido")
     public  String obtenerPagos(){
         return servicePagosCliente.obtenerPagos();
+    }
+
+    @GetMapping("/realizar-pago/{idPedido}")
+    public String realizarPago(@PathVariable Long idPedido) {
+        return servicePagosCliente.realizarPago(idPedido);
     }
 }
--- src/main/java/pe/edu/idat/app_cloud_pedidos_client_ec2/service/ServicePagosCliente.java
+++ src/main/java/pe/edu/idat/app_cloud_pedidos_client_ec2/service/ServicePagosCliente.java
@@ -2,10 +2,15 @@ package pe.edu.idat.app_cloud_pedidos_client_ec2.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "app-cloud-pagos-client-ec2")
 public interface ServicePagosCliente {
 
     @GetMapping("/mensaje")
     String obtenerPagos();
+
+    // Llamará al endpoint en el Servicio de Pagos
+    @GetMapping("/realizar-pago/{idPedido}")
+    String realizarPago(@PathVariable("idPedido") Long idPedido);
 }
