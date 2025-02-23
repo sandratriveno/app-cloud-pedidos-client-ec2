6095d2849b6ef3b5d28dbb994e909d849ccb20ad
sandratriveno <sandra.triveno@gmail.com>
2025-02-23 11:22:16 -0500

    initial Project

---
+++ src/main/java/pe/edu/idat/app_cloud_pedidos_client_ec2/AppCloudPedidosClientEc2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_cloud_pedidos_client_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCloudPedidosClientEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCloudPedidosClientEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_cloud_pedidos_client_ec2/AppCloudPedidosClientEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_cloud_pedidos_client_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCloudPedidosClientEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
